fe2733c759872b8192345c9bd27aa14e0695d6d5
SOUMITRO-SAHA <soumitrosahaofficial@gmail.com>
2024-09-08 13:17:19 +0530

    Implemented: Types

---
+++ apps/frontend/src/types/menu.ts
@@ -0,0 +1,13 @@
+export type MenuGroup = {
+  uniqueId: string;
+  name?: string;
+  items: MenuItem[];
+};
+
+export type MenuItem = {
+  uniqueId: string;
+  name: string;
+  icon?: (className?: string) => React.ReactNode;
+  prefix?: React.ReactNode;
+  postfix?: React.ReactNode;
+};
