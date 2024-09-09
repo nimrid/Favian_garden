768333a990739aaf541280d6c95ba8369ebcb7c5
SOUMITRO-SAHA <soumitrosahaofficial@gmail.com>
2024-09-09 23:25:00 +0530

    Implemented: Right Sige Bar

--- apps/frontend/src/app/(main)/layout.tsx
+++ apps/frontend/src/app/(main)/layout.tsx
@@ -5,6 +5,7 @@ import {
 } from "@/components/ui/resizable";
 import React from "react";
 import { MenuBarComponent } from "./_components/menu-bar";
+import { SideBarComponent } from "./_components/sidebar/sidebar";
 
 interface MainLayoutProps {
   children: React.ReactNode;
@@ -34,8 +35,8 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children, ...props }) => {
         <ResizableHandle />
 
         {/* Right Side Bar */}
-        <ResizablePanel defaultSize={17} minSize={17} maxSize={20}>
-          This is 3rd Side Bar
+        <ResizablePanel defaultSize={20} minSize={20} maxSize={20}>
+          <SideBarComponent />
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
