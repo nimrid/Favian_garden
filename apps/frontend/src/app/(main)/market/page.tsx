8b327a424969f75783c8255a24aa34f7da5a087e
SOUMITRO-SAHA <soumitrosahaofficial@gmail.com>
2024-09-15 02:40:04 +0530

    Adding the Dummy Page Data

---
+++ apps/frontend/src/app/(main)/market/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface MarketPageProps {
+  //Props
+}
+
+const MarketPage: React.FC<MarketPageProps> = (props) => {
+  return <div>MarketPage</div>;
+};
+
+export default MarketPage;
